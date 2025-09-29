48c14a6028fffb0f4adfe50847c1faf6756d7ba2
AkiChase <1003019131@qq.com>
2025-09-29 12:44:48 +0800

    misc: adjust script

--- build.ps1
+++ build.ps1
@@ -7,11 +7,13 @@ $FFMpeg = "ffmpeg-7.1.2"
 $ScriptDir = Get-Location
 $env:PKG_CONFIG_PATH = "$ScriptDir\$FFMpeg\$Prefix\lib\pkgconfig"
 $env:FFMPEG_DIR = "$ScriptDir\$FFMpeg\$Prefix"
-$env:PATH = "$ScriptDir\assets\lib\$OS;$env:PATH"
-$env:RUSTFLAGS = "-C link-args=-Wl,-rpath,`$ORIGIN\assets\$OS"
 
 if ($args[0] -eq "run") {
+    $OldPath = $env:PATH
+    $env:PATH = "$ScriptDir\assets\lib\$OS;$env:PATH"
     cargo run
+    $env:PATH = $OldPath
+    exit 1
 } elseif ($args[0] -eq "release") {
     cd .\frontend
     pnpm build
